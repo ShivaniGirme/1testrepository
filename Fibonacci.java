3f315201f7af4dc02658b1af1b5d0cff0e7938f3
ShivaniGirme <142581596+ShivaniGirme@users.noreply.github.com>
2024-01-19 12:53:06 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+class Fibonacci{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter The Number Of Elements : ");
+		int no=sc.nextInt();
+		int fibo[]=new int[no];
+		 fibo[0]=0;
+		 fibo[1]=1;
+		for (int i=2; i<fibo.length; i++) {
+			fibo[i]=fibo[i-1]+fibo[i-2];
+		}
+		System.out.println(Arrays.toString(fibo));
+	}
+}
+// Enter The Number Of Elements :
+// 5
+// [0, 1, 1, 2, 3]
